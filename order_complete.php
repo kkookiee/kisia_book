e365cce2b56cab9cf9f68723c4cf3cb21bf1f87f
kkookiee <94148698+kkookiee@users.noreply.github.com>
2025-05-16 17:38:55 +0900

    qr 링크 수정

--- order_complete.php
+++ order_complete.php
@@ -15,7 +15,7 @@ $token = $order_id;
 $conn->query("UPDATE orders SET token = '$token', status = 'pending' WHERE id = $order_id");
 
 // 3. QR코드 URL 생성
-$qr_url = "kisia-book.koreacentral.cloudapp.azure.com:8080/pay.php?token=$token";
+$qr_url = "http://kisia-book.koreacentral.cloudapp.azure.com:8080/pay.php?token=$token";
 ?>
 
 <!DOCTYPE html>
