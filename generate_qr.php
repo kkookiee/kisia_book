5dd7b743d7e736b1266899ec6c4f0d3ff16c6962
kkookiee <94148698+kkookiee@users.noreply.github.com>
2025-05-29 19:49:57 +0900

    찐 qr 오류 수정

--- generate_qr.php
+++ generate_qr.php
@@ -3,6 +3,12 @@ require_once 'phpqrcode/qrlib.php';
 
 $data = $_GET['data'] ?? '';
 
+if (!$data) {
+    http_response_code(400);
+    exit('데이터 없음');
+}
+
+$data = urldecode($data);
+
 header('Content-Type: image/png');
 QRcode::png($data);
-?>
